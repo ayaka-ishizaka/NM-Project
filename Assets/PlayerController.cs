6851db6a6cc7d8758eae64df3690b40785e63852
ayaka-ishizaka <lavepersi@outlook.jp>
2020-09-24 19:37:26 +0900

    オリジナルゲームのファイルです。　初めてのコミット

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+    //前身のスピード
+    private float forwardForce = 500.0f;
+    //方向転換のスピード
+    float angleSpeed = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //上矢印キーを押し続けた場合
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //Animatorコンポーネントを取得し、"Run"をtrueにする
+            GetComponent<Animator>().SetTrigger("Run");
+            //前進する
+            this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+        }
+
+        //上矢印を離した場合
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            //Animatorコンポーネントを取得し、"Idle"をtrueにする
+            GetComponent<Animator>().SetTrigger("Idle A");
+            //止まる
+            this.myRigidbody.velocity = Vector3.zero;
+        }
+
+        //左右キーで方向転換
+        float Angle = Input.GetAxisRaw("Horizontal") * Time.deltaTime * angleSpeed;
+        transform.Rotate(Vector3.up * Angle);
+    }
+}
